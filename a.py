dd5513feaf810820a4d6a3c8799e81c5a630f211
plaskod <davidblackfyre1@gmail.com>
2020-11-27 16:12:33 +0100

    1st commit

---
+++ a.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+import collections
+import requests
+
+webpage = 'http://www.ebi.ac.uk/ena/data/view/'
+api='&display=fasta'
+links=[f"{webpage}BM{i:0>6}{api}" for i in range(1,5)]
+
+for link in map(requests.get,links):
+    if link.status_code >= 300:
+        print(f"Failed to open: {file.status_code}")
+        continue
+
+    lines=link.iter_lines()
+    next(lines)
+    neucl_count=collections.defaultdict(int)
+    for line in lines:
+        for c in line:
+            neucl_count[c]+=1
+
+    print("Neuclotides counts: ")
+    print(*(f"{chr(neuclotide)} : {count}" for (neuclotide, count) in neucl_count.items()),sep='\n')
